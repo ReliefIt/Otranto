ef01b2a9258cf18c2b73cd79c45bbbeeca06e3e2
Juan Marcos Lorenzo <111320092+marcoslorenzodev@users.noreply.github.com>
2024-02-26 22:12:23 -0300

    pipelines

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://otranto.pages.github.io',
   fonts: {
     roboto: ['fonts/Roboto-Regular.ttf', 'fonts/Roboto-Bold.ttf', 'fonts/Roboto-Italic.ttf', 'fonts/Roboto-BoldItalic.ttf']
   },
