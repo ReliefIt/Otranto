819faf93a515090287049958ac3b7a0c3352a5f4
Jonathan Ugarte <jona.ugarte10@gmail.com>
2024-03-07 10:21:04 -0300

    Vinculo de boton header a seccion slide , fix bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,9 @@ export default defineConfig({
   fonts: {
     roboto: ['fonts/Roboto-Regular.ttf', 'fonts/Roboto-Bold.ttf', 'fonts/Roboto-Italic.ttf', 'fonts/Roboto-BoldItalic.ttf']
   },
+  
   integrations: []
+  
+  
 });
+
